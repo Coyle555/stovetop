296feee19254f913ae413c88a00d95883b31d2f3
Gorilla Jones <kgorski90lol@gmail.com>
2024-06-03 16:27:26 -0500

    functionality

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,17 +1,44 @@
+"use client"
+
+
+import { useState } from "react";
 
 
 export default function Home() {
+  const [number, setNumber] = useState(0);
+
+  const add = () => {
+    setNumber(number + 1);
+  };
+
+  const subtract = () => {
+    if (number > 0) {
+      setNumber(number - 1);
+    }
+  };
+
   return (
-   
-<main className="bg-black-100 flex items-center justify-center min-h-screen">
-    <div className="container mx-auto p-4">
+    <div className="bg-black-100 flex items-center justify-center min-h-screen">
+      <div className="container mx-auto p-4">
         <div className="flex items-center justify-center space-x-4">
-            <button className="bg-red-500 text-white rounded-full h-12 w-12 flex items-center justify-center text-2xl">-</button>
-            <div className="text-4xl font-bold">0</div>
-            <button  className="bg-green-500 text-white rounded-full h-12 w-12 flex items-center justify-center text-2xl">+</button>
+          <button
+            onClick={subtract}
+            className="bg-red-500 text-white rounded-full h-12 w-12 flex items-center justify-center text-2xl"
+          >
+            -
+          </button>
+          <div className="text-4xl font-bold">{number}</div>
+          <button
+            onClick={add}
+            className="bg-green-500 text-white rounded-full h-12 w-12 flex items-center justify-center text-2xl"
+          >
+            +
+          </button>
         </div>
+      </div>
     </div>
-</main>
-    
   );
 }
+
+
+
